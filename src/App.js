597c925b75aa1410e5ec5f1bb3faed53f67d718e
Emilly <emillymarques96622@gmail.com>
2022-10-07 21:12:00 -0300

    instalação de axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Modal } from './components/modal';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ola queridinhas</h1>
+        <Modal></Modal>
       </header>
     </div>
   );
--- src/service/client.js
+++ src/service/client.js
@@ -1,4 +1,5 @@
-import axios from 'axios'
+import axios from 'axios';
+
 /* import env from 'react-dotenv'
 
 const getURLBackend = () => {
